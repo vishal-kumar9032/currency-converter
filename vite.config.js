446444c53b6daafd385e5e6f877a9d60cbd98739
vishal kumar <vishalkumar9032374977@gmail.com>
2026-06-30 15:24:38 +0530

    new deploy.yml script

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { env } from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: env.BASE_PATH || '/',
   plugins: [react(),
      tailwindcss(),
   ],
