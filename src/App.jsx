48996a0068e4c940ce832226b8bf5da5dce7ffd4
vishal kumar <vishalkumar9032374977@gmail.com>
2026-06-30 15:12:41 +0530

    initial phase of the project

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import { InputBox } from './conponents';
+import { useCurrencyInfo} from './hooks/usecurrencyinfo.js';
+import './App.css'
+
+function App() {
+  const [amount,setamount]=useState(1);
+  const [from,setfrom]=useState("usd");
+  const [to ,setto]=useState("inr");
+  const [convertamount,setconvertamount]=useState(0);
+  const currencyInfo=useCurrencyInfo(from);
+  const options=Object.keys(currencyInfo);
+
+  const swap = () => {
+    setfrom(to);
+    setto(from);
+    setconvertamount(amount);
+    setamount(convertamount);
+  }
+
+  const convert = () => {
+    setconvertamount(amount * currencyInfo[to]);
+  }
+
+
+    
+
+    return (
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://c8.alamy.com/comp/GDJPF4/abstract-finance-world-currency-background-GDJPF4.jpg')`
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert()
+                           
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                currencyoption={options}
+                                oncurrencychange={(currency) => setfrom(currency)}
+                                onAmountChange={(amount) => setamount(amount)}
+                                selectcurrency={from}
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                               
+                                label="To"
+                                amount={convertamount}
+                                currencyoption={options}
+                                oncurrencychange={(currency) => setto(currency)} // This line was missing
+                                selectcurrency={to}
+                                onAmountChange={(amount) => setconvertamount(amount)}
+                                amountDisable={true}
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert {from.toUpperCase()} to {to.toUpperCase()}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App
---
+++ src/hooks/usecurrencyinfo.js
@@ -0,0 +1,16 @@
+import { useEffect ,useState} from "react";
+
+function useCurrencyInfo(currency){
+    const[data,setdata]=useState({});
+    useEffect(()=>{
+              fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.min.json`)
+            .then((res)=>res.json())
+            .then((res)=>setdata(res[currency]))
+
+            
+    },[currency])
+    console.log(data)
+    return data
+}
+  export { useCurrencyInfo };
+  export default useCurrencyInfo;
